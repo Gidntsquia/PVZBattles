ec0ee7444e1badcf04ab5f6ef11902c9e0446104
GidntSquia <jaxondlee@gmail.com>
2022-05-13 16:50:42 -0400

    Add garden manager

---
+++ GardenManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GardenManager : MonoBehaviour
+{
+    public GameObject[] minions = new GameObject[6];
+    public ZombieSpawner zombieSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    private void OnMouseEnter() 
+    {
+        /* Instantiate whatever plant/zombie was selected */
+    }
+
+    private void OnMouseDown() 
+    {
+        /* Place plant/zombie */
+        
+    }
+
+    private void SelectMinion(int zombiePos)
+    {
+        zombieSpawner.
+    }
+
+    
+}
